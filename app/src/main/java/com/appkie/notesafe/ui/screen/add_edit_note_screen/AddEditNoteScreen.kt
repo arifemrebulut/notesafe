d9e41b3878fbc22ded79c8e9368be253776a02c8
Arif Emre Bulut <arifemrebulut@gmail.com>
2022-05-09 00:22:40 +0300

    AddEditNoteScreen setup, screen composable and viewmodel created

---
+++ app/src/main/java/com/appkie/notesafe/ui/screen/add_edit_note_screen/AddEditNoteScreen.kt
@@ -0,0 +1,133 @@
+package com.appkie.notesafe.ui.screen.add_edit_note_screen
+
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.*
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
+import androidx.navigation.NavController
+import com.appkie.notesafe.ui.components.AddEditSettingsSection
+import com.appkie.notesafe.ui.components.AddEditTopBar
+import com.appkie.notesafe.ui.components.DeleteDialog
+import com.appkie.notesafe.ui.navigation.Screen
+
+@Composable
+fun AddEditNoteScreen(
+    navController: NavController,
+    addEditNoteViewModel: AddEditNoteViewModel
+) {
+    val id = addEditNoteViewModel.id
+    val titleState = addEditNoteViewModel.title
+    val descriptionState = addEditNoteViewModel.description
+
+    var showDeleteDialog by remember { mutableStateOf(false) }
+
+    Scaffold(
+        modifier = Modifier
+            .padding(8.dp),
+        topBar = {
+            AddEditTopBar(
+                onBackClicked = {
+                    navController.navigateUp()
+                },
+                onDeleteClicked = {
+                    showDeleteDialog = true
+                },
+                onSaveClicked = {
+                    addEditNoteViewModel.saveNote()
+                    navController.navigate(Screen.HomeScreen.route)
+                },
+                onShareClicked = {
+
+                }
+            )
+        }
+    ) {
+        if (showDeleteDialog) {
+            DeleteDialog(
+                onDismiss = { showDeleteDialog = false },
+                onCancelClicked = { showDeleteDialog = false },
+                onDeleteClicked = {
+                    if (id != -1) {
+                        addEditNoteViewModel.deleteNote()
+                    }
+                    navController.navigate(Screen.HomeScreen.route)
+                }
+            )
+        }
+
+        Column(
+            modifier = Modifier
+                .fillMaxWidth(),
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.Top
+        ) {
+            AddEditSettingsSection()
+            NoteContent(
+                titleState = titleState,
+                onTitleChange = {
+                    addEditNoteViewModel.title = it
+                },
+                descriptionState = descriptionState,
+                onDescriptionChange = {
+                    addEditNoteViewModel.description = it
+                }
+            )
+        }
+    }
+}
+
+@Composable
+fun NoteContent(
+    titleState: String,
+    onTitleChange: (String) -> Unit,
+    descriptionState: String,
+    onDescriptionChange: (String) -> Unit
+) {
+    Column(
+        modifier = Modifier
+            .fillMaxSize(),
+        verticalArrangement = Arrangement.Top,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        TextField(
+            value = titleState,
+            onValueChange = { newText ->
+                onTitleChange(newText)
+            },
+            modifier = Modifier
+                .fillMaxWidth(),
+            placeholder = {
+                Text(
+                    text = "Title",
+                    style = MaterialTheme.typography.h6
+                )
+            },
+            colors = TextFieldDefaults.textFieldColors(
+                backgroundColor = Color.White,
+                focusedIndicatorColor = Color.Transparent,
+                unfocusedIndicatorColor = Color.Transparent
+            ),
+            textStyle = MaterialTheme.typography.h6
+        )
+
+        TextField(
+            value = descriptionState,
+            onValueChange = { newText ->
+                onDescriptionChange(newText)
+            },
+            modifier = Modifier
+                .fillMaxSize(),
+            placeholder = {
+                Text(text = "Note description starts here")
+            },
+            colors = TextFieldDefaults.textFieldColors(
+                backgroundColor = Color.White,
+                focusedIndicatorColor = Color.Transparent,
+                unfocusedIndicatorColor = Color.Transparent
+            )
+        )
+    }
+}
---
+++ app/src/main/java/com/appkie/notesafe/ui/screen/add_edit_note_screen/AddEditNoteViewModel.kt
@@ -0,0 +1,59 @@
+package com.appkie.notesafe.ui.screen.add_edit_note_screen
+
+import androidx.compose.runtime.*
+import androidx.lifecycle.SavedStateHandle
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.appkie.notesafe.data.model.Note
+import com.appkie.notesafe.data.repository.NoteRepository
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.launch
+import javax.inject.Inject
+
+@HiltViewModel
+class AddEditNoteViewModel @Inject constructor(
+    private val noteRepository: NoteRepository,
+    private val savedStateHandle: SavedStateHandle
+) : ViewModel() {
+
+    var id by mutableStateOf(-1)
+    var title by mutableStateOf("")
+    var description by mutableStateOf("")
+
+    init {
+        getNoteIdFromStateHandle()
+        getNoteById()
+    }
+
+    private fun getNoteIdFromStateHandle() {
+        savedStateHandle.get<Int>("noteId")?.let { noteId ->
+            id = noteId
+        }
+    }
+
+    private fun getNoteById() {
+
+    }
+
+    fun saveNote() {
+        viewModelScope.launch {
+            val note = Note(
+                id = id,
+                title = title,
+                description = description
+            )
+            noteRepository.saveNote(note = note)
+        }
+    }
+
+    fun deleteNote() {
+        viewModelScope.launch {
+            val note = Note(
+                id = id,
+                title = title,
+                description = description
+            )
+            noteRepository.deleteNote(note = note)
+        }
+    }
+}
