92bab3ba2c85e929d452e9e97b781bd9abc0c073
Arif Emre Bulut <arifemrebulut@gmail.com>
2022-05-09 02:16:52 +0300

    Pass note and todo id through navigation arguments

--- app/src/main/java/com/appkie/notesafe/ui/navigation/Navigation.kt
+++ app/src/main/java/com/appkie/notesafe/ui/navigation/Navigation.kt
@@ -2,8 +2,10 @@ package com.appkie.notesafe.ui.navigation
 
 import androidx.compose.runtime.Composable
 import androidx.navigation.NavHostController
+import androidx.navigation.NavType
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
+import androidx.navigation.navArgument
 import com.appkie.notesafe.ui.screen.add_edit_note_screen.AddEditNoteScreen
 import com.appkie.notesafe.ui.screen.add_edit_todo_screen.AddEditTodoScreen
 import com.appkie.notesafe.ui.screen.home_screen.HomeScreen
@@ -20,11 +22,22 @@ fun Navigation(
             HomeScreen(navController = navController)
         }
 
-        composable(route = Screen.AddEditNoteScreen.route + "/{noteId}") {
+        composable(
+            route = Screen.AddEditNoteScreen.route + "?noteId={noteId}",
+            arguments = listOf(navArgument(name = "noteId") {
+                type = NavType.IntType
+                defaultValue = -1
+            })
+        ) {
             AddEditNoteScreen(navController = navController)
         }
 
-        composable(route = Screen.AddEditTodoScreen.route + "/{todoId}") {
+        composable(route = Screen.AddEditTodoScreen.route + "?todoId={todoId}",
+            arguments = listOf(navArgument(name = "todoId") {
+                type = NavType.IntType
+                defaultValue = -1
+            })
+        ) {
             AddEditTodoScreen(navController = navController)
         }
     }
