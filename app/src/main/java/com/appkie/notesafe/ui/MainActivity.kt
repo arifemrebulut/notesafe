a1979546aed373806c7d4183ff1521204410e210
Arif Emre Bulut <arifemrebulut@gmail.com>
2022-05-09 02:19:32 +0300

    Call Navigation from MainActivity

--- app/src/main/java/com/appkie/notesafe/ui/MainActivity.kt
+++ app/src/main/java/com/appkie/notesafe/ui/MainActivity.kt
@@ -3,14 +3,20 @@ package com.appkie.notesafe.ui
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.navigation.compose.rememberNavController
+import com.appkie.notesafe.ui.navigation.Navigation
 import com.appkie.notesafe.ui.theme.NotesafeTheme
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             NotesafeTheme {
-
+                val navController = rememberNavController()
+                
+                Navigation(navController = navController)
             }
         }
     }
