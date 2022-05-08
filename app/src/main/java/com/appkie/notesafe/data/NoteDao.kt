c44291cdf0f7f54fbea26b37ee05eca2f2060611
Arif Emre Bulut <arifemrebulut@gmail.com>
2022-05-08 15:51:55 +0300

    Create Note and Todo Dao's for room

---
+++ app/src/main/java/com/appkie/notesafe/data/NoteDao.kt
@@ -0,0 +1,21 @@
+package com.appkie.notesafe.data
+
+import androidx.room.*
+import com.appkie.notesafe.data.model.Note
+import kotlinx.coroutines.flow.Flow
+
+@Dao
+interface NoteDao {
+
+    @Query("SELECT * FROM note_table ORDER BY id ASC")
+    fun getAllNotes(): Flow<List<Note>>
+
+    @Query("SELECT * FROM note_table WHERE id = :noteId")
+    suspend fun getNoteById(noteId: Int): Note?
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun saveNote(note: Note)
+
+    @Delete
+    suspend fun deleteNote(note: Note)
+}
---
+++ app/src/main/java/com/appkie/notesafe/data/TodoDao.kt
@@ -0,0 +1,22 @@
+package com.appkie.notesafe.data
+
+import androidx.room.*
+import com.appkie.notesafe.data.model.Note
+import com.appkie.notesafe.data.model.Todo
+import kotlinx.coroutines.flow.Flow
+
+@Dao
+interface TodoDao {
+
+    @Query("SELECT * FROM todo_table ORDER BY id ASC")
+    fun getAllTodos(): Flow<List<Todo>>
+
+    @Query("SELECT * FROM todo_table WHERE id = :todoId")
+    suspend fun getTodoById(todoId: Int): Todo?
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun saveTodo(todo: Todo)
+
+    @Delete
+    suspend fun deleteTodo(todo: Todo)
+}
