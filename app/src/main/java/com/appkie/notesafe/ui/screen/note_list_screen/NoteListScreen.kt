9fd409c22bb31f0e84550d178e3c366b6d004708
Arif Emre Bulut <arifemrebulut@gmail.com>
2022-05-10 19:24:35 +0300

    Add scaffold to NoteListScreen

--- app/src/main/java/com/appkie/notesafe/ui/screen/note_list_screen/NoteListScreen.kt
+++ app/src/main/java/com/appkie/notesafe/ui/screen/note_list_screen/NoteListScreen.kt
@@ -1,11 +1,14 @@
 package com.appkie.notesafe.ui.screen.note_list_screen
 
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.PaddingValues
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.material.FabPosition
+import androidx.compose.material.FloatingActionButton
+import androidx.compose.material.Icon
+import androidx.compose.material.Scaffold
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.outlined.Add
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
@@ -13,6 +16,7 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.NavController
+import com.appkie.notesafe.ui.components.HomeTopBar
 import com.appkie.notesafe.ui.components.SortingSettingsBar
 import com.appkie.notesafe.ui.navigation.Screen
 import com.appkie.notesafe.ui.screen.note_list_screen.components.NoteCard
@@ -22,27 +26,51 @@ fun NoteListScreen(
     navController: NavController,
     noteListViewModel: NoteListViewModel = hiltViewModel(),
 ) {
+
     val noteList by noteListViewModel.allNotes
 
-    Column(
-        modifier = Modifier
-            .padding(horizontal = 12.dp, vertical = 6.dp),
-        horizontalAlignment = Alignment.CenterHorizontally
-    ) {
-        SortingSettingsBar(
-            modifier = Modifier
-                .fillMaxWidth()
-                .padding(bottom = 12.dp)
-        )
-
-        LazyColumn(
-            contentPadding = PaddingValues(top = 1.dp, bottom = 52.dp)
-        ) {
-            items(noteList) { item ->
-                NoteCard(
-                    note = item,
-                    onNoteClicked = { navController.navigate(Screen.AddEditNoteScreen.route + "/{${item.id}}") }
+    Scaffold(
+        topBar = {
+            HomeTopBar(
+                modifier = Modifier
+                    .padding(horizontal = 12.dp, vertical = 6.dp)
+            )
+        },
+        floatingActionButton = {
+            FloatingActionButton(
+                onClick = {
+                    navController.navigate(Screen.AddEditNoteScreen.route + "/-1")
+                },
+                modifier = Modifier.offset(y = (-56).dp)
+            ) {
+                Icon(
+                    imageVector = Icons.Outlined.Add,
+                    contentDescription = "New Note"
                 )
+            }
+        },
+        floatingActionButtonPosition = FabPosition.End
+    ) {
+        Column(
+            modifier = Modifier
+                .padding(horizontal = 12.dp, vertical = 6.dp),
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            SortingSettingsBar(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(bottom = 12.dp)
+            )
+
+            LazyColumn(
+                contentPadding = PaddingValues(top = 1.dp, bottom = 52.dp)
+            ) {
+                items(noteList) { item ->
+                    NoteCard(
+                        note = item,
+                        onNoteClicked = { navController.navigate(Screen.AddEditNoteScreen.route + "/{${item.id}}") }
+                    )
+                }
             }
         }
     }
