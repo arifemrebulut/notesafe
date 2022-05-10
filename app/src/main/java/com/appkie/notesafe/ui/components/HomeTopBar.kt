3a97801493a5e60a9477baf48313f1070ac5953e
Arif Emre Bulut <arifemrebulut@gmail.com>
2022-05-10 13:33:49 +0300

    Make home screen components global reusable components

--- app/src/main/java/com/appkie/notesafe/ui/components/HomeTopBar.kt
+++ app/src/main/java/com/appkie/notesafe/ui/components/HomeTopBar.kt
@@ -1,4 +1,4 @@
-package com.appkie.notesafe.ui.screen.home_screen.components
+package com.appkie.notesafe.ui.components
 
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
--- app/src/main/java/com/appkie/notesafe/ui/components/SortingSettingsBar.kt
+++ app/src/main/java/com/appkie/notesafe/ui/components/SortingSettingsBar.kt
@@ -1,4 +1,4 @@
-package com.appkie.notesafe.ui.screen.home_screen.components
+package com.appkie.notesafe.ui.components
 
 import androidx.compose.foundation.*
 import androidx.compose.foundation.layout.*
