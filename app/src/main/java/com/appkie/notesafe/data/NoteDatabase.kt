93c56cac2f33b183317f690238e069156b4407dc
Arif Emre Bulut <arifemrebulut@gmail.com>
2022-05-12 21:43:49 +0300

    Increase database version

--- app/src/main/java/com/appkie/notesafe/data/NoteDatabase.kt
+++ app/src/main/java/com/appkie/notesafe/data/NoteDatabase.kt
@@ -10,7 +10,7 @@ import com.appkie.notesafe.data.model.Todo
         Note::class,
         Todo::class
     ],
-    version = 3,
+    version = 4,
     exportSchema = false
 )
 abstract class NoteDatabase : RoomDatabase() {
