48ed9dc44c4ab84b313d9a6b8c7f5bd8897e92a3
Arif Emre Bulut <arifemrebulut@gmail.com>
2022-05-13 19:01:20 +0300

    Add new category entity to database

--- app/src/main/java/com/appkie/notesafe/data/NoteDatabase.kt
+++ app/src/main/java/com/appkie/notesafe/data/NoteDatabase.kt
@@ -2,19 +2,22 @@ package com.appkie.notesafe.data
 
 import androidx.room.Database
 import androidx.room.RoomDatabase
+import com.appkie.notesafe.data.model.Category
 import com.appkie.notesafe.data.model.Note
 import com.appkie.notesafe.data.model.Todo
 
 @Database(
     entities = [
         Note::class,
-        Todo::class
+        Todo::class,
+        Category::class
     ],
-    version = 4,
+    version = 5,
     exportSchema = false
 )
 abstract class NoteDatabase : RoomDatabase() {
 
     abstract val noteDao: NoteDao
     abstract val todoDao: TodoDao
+    abstract val categoryDao: CategoryDao
 }
--- app/src/main/java/com/appkie/notesafe/util/Consts.kt
+++ app/src/main/java/com/appkie/notesafe/util/Consts.kt
@@ -8,4 +8,5 @@ object Consts {
     const val DATABASE_NAME = "note_database"
     const val NOTE_TABLE = "note_table"
     const val TODO_TABLE = "todo_table"
+    const val CATEGORY_TABLE = "category_table"
 }
