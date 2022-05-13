997795d46e6764d84a8d24b360b21f17e2acc98a
Arif Emre Bulut <arifemrebulut@gmail.com>
2022-05-13 23:24:01 +0300

    Show no content illustrations on HomePage if note,todo lists empty

--- app/src/main/java/com/appkie/notesafe/ui/screen/home_screen/notes_tab/NoteListTab.kt
+++ app/src/main/java/com/appkie/notesafe/ui/screen/home_screen/notes_tab/NoteListTab.kt
@@ -1,14 +1,21 @@
 package com.appkie.notesafe.ui.screen.home_screen.notes_tab
 
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.material.ContentAlpha
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Text
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.NavController
+import com.appkie.notesafe.R
 import com.appkie.notesafe.data.model.Note
 import com.appkie.notesafe.ui.components.SortingSettingsBar
 import com.appkie.notesafe.ui.navigation.Screen
@@ -42,17 +49,48 @@ fun NoteListScreen(
             }
         )
 
-        LazyColumn(
-            contentPadding = PaddingValues(top = 1.dp, bottom = 52.dp)
-        ) {
+        if (notes.isNotEmpty()) {
+            LazyColumn(
+                contentPadding = PaddingValues(top = 1.dp, bottom = 52.dp)
+            ) {
 
-            val noteList: List<Note> = if (searching) searchedNotes else notes
+                val noteList: List<Note> = if (searching) searchedNotes else notes
 
-            items(noteList) { item ->
-                NoteCard(
-                    note = item,
-                    onNoteClicked = { navController.navigate(Screen.AddEditNoteScreen.route + "/${item.id}") }
-                )
+                items(noteList) { item ->
+                    NoteCard(
+                        note = item,
+                        onNoteClicked = { navController.navigate(Screen.AddEditNoteScreen.route + "/${item.id}") }
+                    )
+                }
+            }
+        } else {
+            Box(
+                modifier = Modifier
+                    .fillMaxSize(),
+                contentAlignment = Alignment.Center
+            ) {
+                Column(
+                    modifier = Modifier
+                        .offset(y = (-52).dp),
+                    horizontalAlignment = Alignment.CenterHorizontally,
+                    verticalArrangement = Arrangement.Center
+                ) {
+                    Image(
+                        painter = painterResource(id = R.drawable.ic_no_notes),
+                        contentDescription = null,
+                        modifier = Modifier
+                            .width(200.dp)
+                            .height(200.dp)
+                    )
+
+                    Spacer(modifier = Modifier.height(16.dp))
+
+                    Text(
+                        text = "Create Your First Note!",
+                        style = MaterialTheme.typography.h5,
+                        color = Color.Black.copy(alpha = ContentAlpha.medium)
+                    )
+                }
             }
         }
     }
--- app/src/main/java/com/appkie/notesafe/ui/screen/home_screen/todos_tab/TodoListTab.kt
+++ app/src/main/java/com/appkie/notesafe/ui/screen/home_screen/todos_tab/TodoListTab.kt
@@ -1,18 +1,22 @@
 package com.appkie.notesafe.ui.screen.home_screen.todos_tab
 
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.PaddingValues
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.material.ContentAlpha
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.NavController
+import com.appkie.notesafe.R
 import com.appkie.notesafe.ui.components.SortingSettingsBar
 import com.appkie.notesafe.ui.navigation.Screen
 
@@ -45,25 +49,56 @@ fun TodoListScreen(
             }
         )
 
-        LazyColumn(
-            contentPadding = PaddingValues(top = 1.dp, bottom = 52.dp)
-        ) {
+        if (todos.isNotEmpty()) {
+            LazyColumn(
+                contentPadding = PaddingValues(top = 1.dp, bottom = 52.dp)
+            ) {
 
-            val todoList = if (searching) searchedTodos else todos
+                val todoList = if (searching) searchedTodos else todos
 
-            items(todoList) { item ->
-                TodoCard(
-                    todo = item,
-                    onCheckedChange = {
-                        todoListViewModel.onEvent(
-                            TodoListUiEvent.CheckedChange(
-                                todo = item,
-                                checked = it
+                items(todoList) { item ->
+                    TodoCard(
+                        todo = item,
+                        onCheckedChange = {
+                            todoListViewModel.onEvent(
+                                TodoListUiEvent.CheckedChange(
+                                    todo = item,
+                                    checked = it
+                                )
                             )
-                        )
-                    },
-                    onTodoClicked = { navController.navigate(Screen.AddEditTodoScreen.route + "/${item.id}") }
-                )
+                        },
+                        onTodoClicked = { navController.navigate(Screen.AddEditTodoScreen.route + "/${item.id}") }
+                    )
+                }
+            }
+        } else {
+            Box(
+                modifier = Modifier
+                    .fillMaxSize(),
+                contentAlignment = Alignment.Center
+            ) {
+                Column(
+                    modifier = Modifier
+                        .offset(y = (-52).dp),
+                    horizontalAlignment = Alignment.CenterHorizontally,
+                    verticalArrangement = Arrangement.Center
+                ) {
+                    Image(
+                        painter = painterResource(id = R.drawable.ic_no_todos),
+                        contentDescription = null,
+                        modifier = Modifier
+                            .width(200.dp)
+                            .height(200.dp)
+                    )
+
+                    Spacer(modifier = Modifier.height(16.dp))
+
+                    Text(
+                        text = "Create Your First Todos!",
+                        style = MaterialTheme.typography.h5,
+                        color = Color.Black.copy(alpha = ContentAlpha.medium)
+                    )
+                }
             }
         }
     }
